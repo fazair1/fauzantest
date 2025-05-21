60c59ae14af3b8d54360c24d3be0d4aa64dfa85f
fazair1 <fauzan.irfanto1@gmail.com>
2025-05-21 16:17:47 +0700

    Initial commit

---
+++ src/main/java/com/fauzan/fauzantest/FauzantestApplication.java
@@ -0,0 +1,13 @@
+package com.fauzan.fauzantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FauzantestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FauzantestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fauzan/fauzantest/FauzantestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fauzan.fauzantest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FauzantestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
